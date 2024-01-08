cd017c322bbbac56df39c7efa4e70bd1dd163f20
Joon Lee <joonsauce@gmail.com>
2024-01-08 10:07:22 -0500

    Settings: Update comments and imports for better readability

--- settings.py
+++ settings.py
@@ -1,21 +1,19 @@
 import asyncio
-import time
-import discord
-import os
-import requests
-import pandas
-import json
-from discord.ext import commands
 import csv
-import matplotlib.pyplot as plt
+import discord
 import itertools
+import matplotlib.pyplot as plt
+import os
+import pandas
+import requests
+from discord.ext import commands
 
 prefix = "d!"
 description = "Joon's updated version of DisGraph, a HTN 2020++ project and submission by Team Joon."
 # csv and xlsx/xls should be enough and most common enough
 supported_file_types = [".csv", ".xlsx", ".xls"]
 
-# discord gateway intents
+# only default intents required
 intents = discord.Intents.default()
 allowed_mentions = discord.AllowedMentions(everyone=False,
                                            users=True,
