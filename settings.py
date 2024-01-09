a379d90be8d1fa7deab53dbc23374c3b8f7de554
Joon Lee <joonsauce@gmail.com>
2024-01-09 17:26:44 -0500

    Settings: Add skeleton for ready function

--- settings.py
+++ settings.py
@@ -25,3 +25,8 @@ bot = commands.Bot(command_prefix=prefix, intents=intents, description=descripti
 
 with open('bot_token.txt', 'r') as token:
     bot_token = token.readline()
+
+@bot.event
+async def on_ready():
+    print("Status: OK")
+
