385a2e9e7a1212fafb70f15e11a6eff9b74de6e1
Joon Lee <joonsauce@gmail.com>
2024-01-09 22:58:06 -0500

    Bot: Remove graph command, replace with graph_file command

--- bot.py
+++ bot.py
@@ -1,125 +1,27 @@
-import asyncio
-import time
-import discord
-import pandas
-import json
-from discord.ext import commands
-from graph import *
-from dropbase import *
+from functions import *
 from settings import *
 
-# discord gateway intents
-intents = discord.Intents.default()
-allowed_mentions = discord.AllowedMentions(everyone=False,
-                                           users=True,
-                                           roles=False)
-
-# bot instance
-bot = discord.ext.commands.Bot(command_prefix=prefix,
-                               intents=intents,
-                               description=description,
-                               case_insensitive=True,
-                               allowed_mentions=allowed_mentions)
-
-
+# graph_file command: given a file, analyzes data and graphs it
 @bot.command()
-async def graph(ctx, graph_type):
-    """
-    {graph_type} Renders a desired graph type from a csv dataset
-    """
-    await ctx.send(f"Send a file to be a graphed. It must be a {', '.join(supported_file_types)}")
+async def graph_file(ctx):
+    await ctx.send(f"Please upload a file with `{','.join(supported_file_types)}` file extensions")
+    msg = await wait_for_message(ctx)
+    if msg:
+        if msg.attachments:
+            file_name = msg.attachments[0].filename
+            file_id = uuid.uuid4()
+            file_path = 'temp/{0}.csv'.format(file_id)
+            if any([file_name.endswith(i) for i in supported_file_types]):
+                if not file_name.endswith('.csv'):
+                    convert_to_csv(msg.attachments)
+                else:
+                    await msg.attachments[0].save(file_path)
+                await initial_analysis(ctx, file_path, file_id)
+                os.remove(file_path)
+                os.remove('temp/{0}.png'.format(file_id))
+            else:
+                await ctx.send("Invalid file type. Please attach a file with `{0}` extensions and try again.".format(','.join(supported_file_types)))
+        else:
+            await ctx.send("No files attached. Please try again.")
 
-    def check(message):
-        return message.author == ctx.author and len(message.attachments) == 1
-
-    msg = discord.Message
-    try:
-        msg = await bot.wait_for('message', check=check, timeout=60)
-    except asyncio.TimeoutError:
-        await ctx.send('Timed out. Please try again')
-
-    # check if file is supported
-    # FIXME: check for all in the supported file types list, not just .csv
-    if ".csv" not in msg.attachments[0].filename:
-        await ctx.send(f"That's not a valid file type. It must be a {', '.join(supported_file_types)}")
-    else:
-        # saves with filename
-        await msg.attachments[0].save("temp/file.csv")
-
-        # run the pipeline job
-        if graph_type.lower()[0:2] == 'yy':
-            job = Job('X VS YY TABLE')
-            job.run_pipeline()
-            time.sleep(5)
-
-            # saves query as temp json
-            with open('temp/file.json', 'w', encoding='utf-8') as outfile:
-                json.dump(database_query('x_vs_yy', 'x,y1,y2'), outfile,
-                          ensure_ascii=False, indent=4)
-        elif graph_type.lower()[0:2] == 'y-':
-            job = Job('NAME VS COUNT TABLE')
-            job.run_pipeline()
-            time.sleep(5)
-
-            # saves query as temp json
-            with open('temp/file.json', 'w', encoding='utf-8') as outfile:
-                json.dump(database_query('name_vs_count', 'name,count'),
-                          outfile, ensure_ascii=False, indent=4)
-
-        elif graph_type.lower() == 'geodot':
-            job = Job('GEODOT TABLE')
-            job.run_pipeline()
-            time.sleep(5)
-
-            # saves query as temp json
-            with open('temp/file.json', 'w', encoding='utf-8') as outfile:
-                json.dump(database_query('geodot', 'name,lat,lon'),
-                          outfile, ensure_ascii=False, indent=4)
-
-        elif graph_type.lower() == 'geospatial':
-            job = Job('GEOSPATIAL TABLE')
-            job.run_pipeline()
-            time.sleep(10)
-
-            # saves query as temp json
-            with open('temp/file.json', 'w', encoding='utf-8') as outfile:
-                json.dump(database_query('geospatial', 'lon_departure,lat_departure,lon_arrival,lat_arrival'),
-                          outfile, ensure_ascii=False, indent=4)
-
-        # converts json to csv
-        file = pandas.read_json('temp/file.json')
-        file.to_csv('temp/new_file.csv', index=False)
-
-    if graph_type.lower() == 'yy-bar':
-        # bar graph for csv
-        basic_bar('temp/new_file.csv')
-        await ctx.send(file=discord.File(open('temp/bar.png', 'rb'), 'bar.png'))
-
-    elif graph_type.lower() == 'yy-line':
-        # graphs the csv data
-        basic_line('temp/new_file.csv')
-        await ctx.send(file=discord.File(open('temp/Line.png', 'rb'), 'Line.png'))
-
-    elif graph_type.lower() == 'yy-scatter':
-        # graphs the csv data
-        basic_scatter('temp/new_file.csv')
-        await ctx.send(
-            file=discord.File(open('temp/scatter.png', 'rb'), 'scatter.png'))
-
-    elif graph_type.lower() == 'y-pie':
-        basic_pie('temp/new_file.csv')
-        await ctx.send(
-            file=discord.File(open('temp/pie.png', 'rb'), 'pie.png'))
-
-    elif graph_type.lower() == 'geodot':
-        geo_dot('temp/new_file.csv')
-        await ctx.send(
-            file=discord.File(open('temp/map.png', 'rb'), 'map.png'))
-
-    elif graph_type.lower() == 'geospatial':
-        geo_spatial('temp/new_file.csv')
-        await ctx.send(
-            file=discord.File(open('temp/spatial.png', 'rb'), 'spatial.png'))
-
-
-bot.run(os.getenv('BOT_TOKEN'))
+bot.run(bot_token)
