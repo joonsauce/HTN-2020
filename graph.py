b4eb6ee335d87494ab9f4b1067f0eca63d62dad6
Joon Lee <joonsauce@gmail.com>
2024-01-11 19:03:07 -0500

    Graph: Add three-variable line graph, minor comment updates

--- graph.py
+++ graph.py
@@ -1,9 +1,15 @@
 from settings import *
 
 
-def set_plot_info(x_label, y_label, title, file_id):
+def two_var_set_plot_info(x_label, y_label, title, file_id):
     plt.xlabel(x_label)
     plt.ylabel(y_label)
+    plt.title(title)
+    plt.savefig('temp/{}.png'.format(file_id))
+    plt.clf()
+
+
+def three_var_set_plot_info(title, file_id):
     plt.title(title)
     plt.savefig('temp/{}.png'.format(file_id))
     plt.clf()
@@ -18,38 +24,59 @@ def get_two_var_data(file):
     return [x, y]
 
 
+def get_three_var_data(file):
+    with open(file, 'r') as csvfile:
+        data = list(csv.reader(csvfile))
+        row_len = len(data)
+        x = [data[i][0] for i in range(row_len)]
+        y1 = [data[i][1] for i in range(row_len)]
+        y2 = [data[i][2] for i in range(row_len)]
+    return [x, y1, y2]
+
+
 def two_var_line(file, file_id):
     """
-    Renders a basic line graph
+    Renders a two-variable line graph
     :param file: path to file
     :param file_id: id of file
     :return: saves image to temp/{file_id}.png
     """
     data = get_two_var_data(file)
     plt.plot(data[0][1:], data[1][1:])
-    set_plot_info(data[0][0], data[1][0], "{0} vs. {1}".format(data[0][0], data[1][0]), file_id)
+    two_var_set_plot_info(data[0][0], data[1][0], "{0} vs. {1}".format(data[0][0], data[1][0]), file_id)
+
+
+def three_var_line(file, file_id):
+    data = get_three_var_data(file)
+    fig, ax1 = plt.subplots()
+    ax1.set_xlabel(data[0][0])
+    ax1.set_ylabel(data[1][0])
+    ax2 = ax1.twinx()
+    ax2.set_ylabel(data[2][0])
+    plt.plot(data[0][1:], data[1][1:], data[2][1:])
+    three_var_set_plot_info(f"{data[0][0]} vs. {data[1][0]} vs. {data[2][0]}", file_id)
 
 
 def two_var_bar(file, file_id):
     """
-    Renders a basic bar graph
+    Renders a two-variable bar graph
     :param file: path to file
     :return: saves image to temp/{file_id}.png
     """
     data = get_two_var_data(file)
     plt.bar(data[0][1:], data[1][1:])
-    set_plot_info(data[0][0], data[1][0], "{0} vs. {1}".format(data[0][0], data[1][0]), file_id)
+    two_var_set_plot_info(data[0][0], data[1][0], "{0} vs. {1}".format(data[0][0], data[1][0]), file_id)
 
 
 def two_var_scatter(file, file_id):
     """
-    Renders a basic scatter graph
+    Renders a two-variable scatter graph
     :param file: path to file
     :return: saves image to temp/{file_id}.png
     """
     data = get_two_var_data(file)
     plt.scatter(data[0][1:], data[1][1:])
-    set_plot_info(data[0][0], data[1][0], "{0} vs. {1}".format(data[0][0], data[1][0]), file_id)
+    two_var_set_plot_info(data[0][0], data[1][0], "{0} vs. {1}".format(data[0][0], data[1][0]), file_id)
 
 
 def basic_pie(file, file_id):
